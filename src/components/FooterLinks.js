1b09bc7dee3097f685fc6417e60b817dcb63b5f9
Seychelle Koplik <sey.koplik@gmail.com>
2020-11-02 20:36:32 -0500

    changed footer styling

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -6,7 +6,7 @@ function FooterLinks() {
 
   return (
     <footer className="navbar fixed-bottom navbar-expand-lg text-center footer">
-      <p className="navbar-brand m-auto"><small>&#169; Copyright 2020 Seychelle Koplik</small></p>
+      <p className="navbar-brand copyright">&#169; Copyright 2020 Seychelle Koplik</p>
       <h4 className="socialFooter"> Social: 
       <a href="http://www.github.com/seykoplik" target="_blank" className="fa fa-github fafooter"></a>
       <a href="http://linkedin.com/in/seykoplik" target="_blank" className="fa fa-linkedin fafooter"></a>
