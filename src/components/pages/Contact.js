e5b1d1a270e445a7b1422b7b278946f4befd5438
Seychelle Koplik <sey.koplik@gmail.com>
2020-11-02 20:25:22 -0500

    contact form added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,9 +2,28 @@ import React from "react";
 
 function Contact(props) {
   return (
-    <div>
-      <h1>Contact form here!</h1>
-    </div>
+    <>
+      <form>
+        <fieldset>
+          <h3>Contact <small className="text-muted">Seychelle</small></h3>
+          <p>This section is still under construction please bare with me.</p>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your name" />
+              </div>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label for="exampleTextarea">Message</label>
+                <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
+              </div>
+              <button type="submit" className="btn btn-outline-secondary">Submit</button>
+            </fieldset>
+         </form>
+    </>
   );
 }
 
