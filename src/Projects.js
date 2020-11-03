fb971338bb55a1f005b2b71e06d39dd9b0d85d9e
Seychelle Koplik <sey.koplik@gmail.com>
2020-11-02 22:44:23 -0500

    picture still not rendering appropriately

---
+++ src/Projects.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+        "id": 1,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "../../images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 3,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 5,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 6,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 7,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    },
+    {
+        "id": 8,
+        "title": "burgerApp",
+        "repo_url": "https://github.com/SeyKoplik/MySQL-HomemadeORM_BurgerApp",
+        "demo_img": "./images/burgerApp.png",
+        "deployed_url": "https://burgerappbysey.herokuapp.com/"
+    }
+]
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,29 +1,27 @@
 import React from "react";
-import portfolio from "../../portfolio.json";
+import Projects from "../../Projects.js";
 import PortfolioCard from "../PortfolioCard";
 
-class Portfolio extends React.Component {
-  state = {
-    portfolio: portfolio
-  }
-  
-  render() {
+function Portfolio() {
+    const oneProject = Projects.map(oneData => 
+      <PortfolioCard 
+      title={oneData.title}
+      repo_url={oneData.repo_url}
+      demo_img={oneData.demo_img}
+      deployed_url={oneData.deployed_url}
+      key={oneData.id} />
+      )
+
+      console.log(Projects[1].demo_img);
+
   return (
     <>
       <h3>Projects <small className="text-muted">&& Activities</small></h3>
       <hr />
-
-      {this.state.portfolio.map(oneData => 
-        <PortfolioCard 
-        title={oneData.title}
-        repo_url={oneData.repo_url}
-        demo_img={oneData.demo_img}
-        deployed_url={oneData.deployed_url}
-        key={oneData.id} />
-        )}
+      {oneProject}
     </>
-  );
+  )
 }
-}
+
 
 export default Portfolio;
